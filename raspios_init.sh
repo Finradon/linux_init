3c101b85c6416e121fedc0d1d485433f9e108657
Benedict Harder <benedict.harder@tum.de>
2021-12-22 20:12:40 +0100

    updates to raspios script

--- raspios_init.sh
+++ raspios_init.sh
@@ -4,11 +4,9 @@ apt update
 apt upgrade -y
 apt autoremove -y
 
-cat << EOF >> $HOME/.bashrc
-export PS1="\[$(tput bold)\]\[\033[38;5;10m\]\u@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]:\[$(tput sgr0)\] \[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;33m\]\W\[$(tput sgr0)\] \[$(tput bold)\]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
-EOF
+echo "export PS1="\[$(tput bold)\]\[\033[38;5;10m\]\u@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]:\[$(tput sgr0)\] \[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;33m\]\W\[$(tput sgr0)\] \[$(tput bold)\]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]"" >> $HOME/.bashrc
 
-. $HOME/.bashrc
+bash $HOME/.bashrc
 
 apt install -y python3
 apt install -y python3-pip
