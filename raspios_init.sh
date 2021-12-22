6d3cd4f1551fe7437b8b6969715c4b9d436b7025
Benedict Harder <benedict.harder@tum.de>
2021-12-22 20:35:53 +0100

    adds ssh, git config and directories to raspios

--- raspios_init.sh
+++ raspios_init.sh
@@ -10,3 +10,12 @@ bash $HOME/.bashrc
 
 apt install -y python3
 apt install -y python3-pip
+
+mkdir $HOME/.ssh
+mkdir $HOME/repos
+
+ssh-keygen -t rsa -b 2048 -C "rpi" -f $HOME/.ssh/id_rsa -q -N ""
+
+git config --global user.name "Benedict Harder"
+git config --global user.email benedict.harder@tum.de
+git config --global core.editor nano
