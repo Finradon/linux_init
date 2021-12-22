88cb1f9293a1ec9a9289ab7617f0b5aad20d556c
Benedict Harder <benedict.harder@tum.de>
2021-12-22 20:19:38 +0100

    updates to raspios script

--- raspios_init.sh
+++ raspios_init.sh
@@ -4,7 +4,7 @@ apt update
 apt upgrade -y
 apt autoremove -y
 
-echo "export PS1="\[$(tput bold)\]\[\033[38;5;10m\]\u@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]:\[$(tput sgr0)\] \[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;33m\]\W\[$(tput sgr0)\] \[$(tput bold)\]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]"" >> $HOME/.bashrc
+echo 'export PS1="\[$(tput bold)\]\[\033[38;5;10m\]\u@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]:\[$(tput sgr0)\] \[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;33m\]\W\[$(tput sgr0)\] \[$(tput bold)\]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]"' >> $HOME/.bashrc
 
 bash $HOME/.bashrc
 
